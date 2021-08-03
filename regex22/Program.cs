944028f90f516c57aaefa304e5f65b77bb1bfddc
Александр Котов <megalodon2710@gmail.com>
2021-08-03 22:11:31 +0700

    Добавьте файлы проекта.

---
+++ regex22/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regex22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя и фамилию: ");
+
+            string text = Console.ReadLine();
+           string pattern = (@"(^[A-Z?])[a-z]+\s([A-Z?])[a-z]+$");
+            {
+                if (Regex.IsMatch(text, pattern))
+                {
+                    Console.WriteLine("Успешно!");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка:неверный формат");
+                }
+            }
+        }
+    }
+}
